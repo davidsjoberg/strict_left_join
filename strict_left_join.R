f0929505dbdadd26fe241d37379efad8c899cdd2
david sjoberg <dav.sjob@gmail.com>
2020-05-16 19:35:36 +0200

    Fixed strictness types

--- strict_left_join.R
+++ strict_left_join.R
@@ -1,5 +1,7 @@
-`%>%` <- magrittr::`%>%`
+library(dplyr, warn.conflicts = F)
+library(stringr)
 
+### strict_left_join ###
 strict_left_join <- function(x, y, by = NULL, suffix = c("", ".y"), strictness = c("error", "warning", "none")) {
   if(!any(c(is.data.frame(x), is.data.frame(y)))) {stop("'x' and 'y' needs to be data frames")}
   strictness <- match.arg(strictness)
@@ -8,7 +10,10 @@ strict_left_join <- function(x, y, by = NULL, suffix = c("", ".y"), strictness =
     dplyr::mutate(n__ = n()) %>% 
     dplyr::ungroup()
   
-  if(any(y %>% dplyr::pull(n__) > 1)) {
+  if(any(y %>% dplyr::pull(n__) > 1) & strictness != "none") {
+    if(strictness == "error") {
+      stop("Duplicates in the RHS .y.")
+    }
     warning(paste0("Duplicates in .y blocked from join. Duplicates in rows c(", 
                    y %>% 
                      dplyr::mutate(r__ = dplyr::row_number()) %>% 
@@ -16,13 +21,23 @@ strict_left_join <- function(x, y, by = NULL, suffix = c("", ".y"), strictness =
                      dplyr::pull(r__) %>% 
                      paste(collapse = ", "), 
                    ")"))
+    y <- y %>% 
+      dplyr::filter(n__ == 1)
   }
   
   y <- y %>% 
-    dplyr::filter(dplyr::n() == 1) %>% 
     dplyr::select(-n__)
-  
   dplyr::left_join(x, y, by = by, suffix = suffix)
 }
 
-strict_left_join(mtcars, mtcars, strictness = "error")
+### Sample data ###
+x <- tibble(id = 1:3,
+            name = starwars$name[1:3]); x
+
+y <- tibble(id = c(1, 2, 2, 3),
+                favorit_fruit = fruit[1:4]); y
+
+### Function behaviour ###
+strict_left_join(x, y, by = "id", strictness = "error")
+strict_left_join(x, y, by = "id", strictness = "warning")
+strict_left_join(x, y, by = "id", strictness = "none")
